4ad6df83c699426d55318ae33f3c7589a0d90e77
cammichbursc@gmail.com <cammichbursc@gmail.com>
2024-01-11 21:19:49 -0600

    added ds store and node modules to gitignore file

---
+++ index.js
@@ -0,0 +1,28 @@
+// TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
+const generateMarkdown = require('./utils/generateMarkdown');
+
+// TODO: Create an array of questions for user input
+function userInput(){
+    return inquirer.prompt([
+    
+    
+    
+    ])};
+
+// TODO: Create a function to create SVG file
+function writeToFile(fileName, data) {
+    fs.appendFile(`${fileName}.md`, data, 
+    (err) => err ? console.error(err) : console.log(`${fileName}.md has been generated.`))
+}
+
+// TODO: Create a function to initialize app
+async function init() {
+    let answers = await userInput();
+    writeToFile((answers.fileName),(generateMarkdown(answers)));
+}
+
+
+// Function call to initialize app
+init();
